839c1e3fd301def9b9d8d6befaabba0ec0965cb7
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-01-16 14:41:24 -0300

    fix: adiciona PostMapping no método criarCliente()

--- src/main/java/com/zup/pizzaria/controllers/ClienteController.java
+++ src/main/java/com/zup/pizzaria/controllers/ClienteController.java
@@ -4,6 +4,7 @@ import com.zup.pizzaria.models.Cliente;
 import com.zup.pizzaria.services.ClienteService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ClienteController {
     private ClienteService clienteService;
 
+    @PostMapping
     public ResponseEntity<Cliente> criarCliente(@RequestBody Cliente cliente) {
         Cliente novoCliente = clienteService.salvarCliente(cliente);
         return ResponseEntity.ok(novoCliente);

ce243141db1286dd0eab4f4c4a885a70e73c0593
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-01-17 11:33:56 -0300

    feat: adiciona injeção de dependencia na classe ClienteService, além de usar o método de ClienteMapper para conversão do cliente

--- src/main/java/com/zup/pizzaria/services/ClienteService.java
+++ src/main/java/com/zup/pizzaria/services/ClienteService.java
@@ -1,5 +1,7 @@
 package com.zup.pizzaria.services;
 
+import com.zup.pizzaria.dtos.ClienteDTO;
+import com.zup.pizzaria.mappers.ClienteMapper;
 import com.zup.pizzaria.models.Cliente;
 import com.zup.pizzaria.repository.ClienteRepository;
 import org.springframework.stereotype.Service;
@@ -9,7 +11,12 @@ public class ClienteService {
 
     private ClienteRepository clienteRepository;
 
-    public Cliente salvarCliente(Cliente cliente) {
+    public ClienteService(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
+
+    public Cliente salvarCliente(ClienteDTO clienteDTO) {
+        Cliente cliente = ClienteMapper.converterDTOParaModelo(clienteDTO);
         return clienteRepository.save(cliente);
     }
 }

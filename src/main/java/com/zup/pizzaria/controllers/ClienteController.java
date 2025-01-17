90b794697bd305e4ebff7d030c9db500ee823e5e
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-01-17 15:03:58 -0300

    feat: adiciona métodos getById e getAll na classe ClienteService

--- src/main/java/com/zup/pizzaria/controllers/ClienteController.java
+++ src/main/java/com/zup/pizzaria/controllers/ClienteController.java
@@ -5,10 +5,7 @@ import com.zup.pizzaria.models.Cliente;
 import com.zup.pizzaria.services.ClienteService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/clientes")
--- src/main/java/com/zup/pizzaria/services/ClienteService.java
+++ src/main/java/com/zup/pizzaria/services/ClienteService.java
@@ -6,6 +6,9 @@ import com.zup.pizzaria.models.Cliente;
 import com.zup.pizzaria.repository.ClienteRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class ClienteService {
 
@@ -19,5 +22,13 @@ public class ClienteService {
             clienteDTO.validar();
             Cliente cliente = ClienteMapper.converterDTOParaModelo(clienteDTO);
             return clienteRepository.save(cliente);
+    }
+
+    public Optional<Cliente> getById(Long id) {
+        return clienteRepository.findById(id);
+    }
+
+    public List<Cliente> getAll() {
+        return clienteRepository.findAll();
     }
 }
